81067bee52220c7ee907d6717edc2b93b8720ebf
peliksilagan <johnfelixilagan@gmail.com>
2016-02-06 06:53:07 +0800

    Commit - Video #47

--- app/src/main/java/com/peliks/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/peliks/bucketdrops/ActivityMain.java
@@ -2,12 +2,27 @@ package com.peliks.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackground();
+    }
+
+    private void initBackground() {
+        ImageView ivBackground = (ImageView) findViewById(R.id.ivBackground);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(ivBackground);
     }
 }
