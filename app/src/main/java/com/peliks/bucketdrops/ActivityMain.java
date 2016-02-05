f2d1a04694e85f88dadc49ae3644cfb0dad05eb4
peliksilagan <johnfelixilagan@gmail.com>
2016-02-06 07:16:29 +0800

    Commit - Video #47

--- app/src/main/java/com/peliks/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/peliks/bucketdrops/ActivityMain.java
@@ -1,15 +1,19 @@
 package com.peliks.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar mToolbar;
+    private Button mBtnAddDrop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,18 @@ public class ActivityMain extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         initBackground();
+
+        mBtnAddDrop = (Button) findViewById(R.id.btnAddDrop);
+        mBtnAddDrop.setOnClickListener(this);
     }
 
     private void initBackground() {
         ImageView ivBackground = (ImageView) findViewById(R.id.ivBackground);
         Glide.with(this).load(R.drawable.background).centerCrop().into(ivBackground);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(ActivityMain.this, "Button was clicked!", Toast.LENGTH_SHORT).show();
     }
 }
